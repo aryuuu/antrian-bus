1ba730f373c7235f808bfaae9ba1ac157d63089c
Muhammad Fikri Hizbullah <fhizbullah99@gmail.com>
2021-03-12 04:23:21 +0700

    change bus wait in location algorithm

--- src/main.c
+++ src/main.c
@@ -74,60 +74,60 @@ void report() {
       printf(" > TERMINAL 2 ");
 
     sampst(0.0, -(i));
-    printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
-           transfer[2]);
+    printf("(AVG : %.2f s, MAX : %.2f s, MIN : %.2f s)\n", transfer[1], transfer[3],
+           transfer[4]);
   }
 
-  int T, T1, T2;
+  // int T, T1, T2;
 
-  printf("e) BUS LOOP\n");
-  for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
-    T = 0;
-    T = T + i;
+  // printf("e) BUS LOOP\n");
+  // for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
+  //   T = 0;
+  //   T = T + i;
 
-    sampst(0.0, -(T));
-    printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
-           transfer[2]);
-  }
+  //   sampst(0.0, -(T));
+  //   printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
+  //          transfer[2]);
+  // }
 
-  printf("f.1) A PERSON IN THE SYSTEM ARRIVE TO CAR RENTAL\n");
-  for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
-    T1 = 0;
-    T2 = 0;
-    // Terminal 1 ke Car Rental
-    if (i != EVENT_BUS_GO_TO_TERMINAL_1) {
-        T1 = T1 + i;
-        //  Terminal 2 ke Car Rental
-        if ((i != EVENT_PEOPLE_TERMINAL_1) && (i != EVENT_BUS_GO_TO_TERMINAL_2)) {
-            T2 = T2 + i;
-        }
-    }
-    sampst(0.0, -(T1 + T2));
-    printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
-           transfer[2]);
-  }
+  // printf("f.1) A PERSON IN THE SYSTEM ARRIVE TO CAR RENTAL\n");
+  // for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
+  //   T1 = 0;
+  //   T2 = 0;
+  //   // Terminal 1 ke Car Rental
+  //   if (i != EVENT_BUS_GO_TO_TERMINAL_1) {
+  //       T1 = T1 + i;
+  //       //  Terminal 2 ke Car Rental
+  //       if ((i != EVENT_PEOPLE_TERMINAL_1) && (i != EVENT_BUS_GO_TO_TERMINAL_2)) {
+  //           T2 = T2 + i;
+  //       }
+  //   }
+  //   sampst(0.0, -(T1 + T2));
+  //   printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
+  //          transfer[2]);
+  // }
 
-  printf("f.2) A PERSON IN THE SYSTEM ARRIVE TO TERMINAL 1\n");
-  for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
-    T = 0;
-    if ((i == EVENT_PEOPLE_CAR_RENTAL) || (i == EVENT_BUS_GO_TO_TERMINAL_1) || (i == EVENT_PEOPLE_TERMINAL_1)) {
-        T = T + i;
-    }
-    sampst(0.0, -(T));
-    printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
-           transfer[2]);
-  }
+  // printf("f.2) A PERSON IN THE SYSTEM ARRIVE TO TERMINAL 1\n");
+  // for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
+  //   T = 0;
+  //   if ((i == EVENT_PEOPLE_CAR_RENTAL) || (i == EVENT_BUS_GO_TO_TERMINAL_1) || (i == EVENT_PEOPLE_TERMINAL_1)) {
+  //       T = T + i;
+  //   }
+  //   sampst(0.0, -(T));
+  //   printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
+  //          transfer[2]);
+  // }
 
-  printf("f.3) A PERSON IN THE SYSTEM ARRIVE TO TERMINAL 2\n");
-  for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
-    T = 0;
-    if (i != EVENT_BUS_GO_TO_CAR_RENTAL) {
-        T = T + i;
-    }
-    sampst(0.0, -(T));
-    printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
-           transfer[2]);
-  }
+  // printf("f.3) A PERSON IN THE SYSTEM ARRIVE TO TERMINAL 2\n");
+  // for (int i = EVENT_BUS_GO_TO_TERMINAL_1; i <= EVENT_PEOPLE_CAR_RENTAL; i++) {
+  //   T = 0;
+  //   if (i != EVENT_BUS_GO_TO_CAR_RENTAL) {
+  //       T = T + i;
+  //   }
+  //   sampst(0.0, -(T));
+  //   printf("(AVG : %.2f, MAX : %2.f, MIN : %.2f)\n", transfer[1], transfer[3],
+  //          transfer[2]);
+  // }
 }
 
 int main() {
@@ -186,8 +186,7 @@ int main() {
 
       if (bus_location == CAR_RENTAL && bus_num_passenger() < 20) {
         // printf("PC in\n");
-        people_in(CAR_RENTAL);
-        sampst(0.0, NUMBER_QUEUE + CAR_RENTAL - 1);
+        people_in(CAR_RENTAL, bus_wait_time);
       } else {
         // printf("PC q\n");
         people_queue(CAR_RENTAL);
@@ -201,8 +200,7 @@ int main() {
 
       if (bus_location == TERMINAL_1 && bus_num_passenger() < 20) {
         // printf("PT1 in\n");
-        people_in(TERMINAL_1);
-        sampst(0.0, NUMBER_QUEUE + TERMINAL_1 - 1);
+        people_in(TERMINAL_1, bus_wait_time);
       } else {
         // printf("PT1 q\n");
         people_queue(TERMINAL_1);
@@ -215,8 +213,7 @@ int main() {
                      EVENT_PEOPLE_TERMINAL_2);
       if (bus_location == TERMINAL_2 && bus_num_passenger() < 20) {
         // printf("PT2 in\n");
-        people_in(TERMINAL_2);
-        sampst(0.0, NUMBER_QUEUE + TERMINAL_2 - 1);
+        people_in(TERMINAL_2, bus_wait_time);
       } else {
         // printf("PT2 q\n");
         people_queue(TERMINAL_2);
